9bd1f3540d66e5cc40d988588db95c91b21562c3
Alec Mitchell <alecpm@gmail.com>
2022-05-27 18:49:50 -0700

    Add utils tests.

---
+++ src/jazkarta/zoterolib/tests/test_utils.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import unittest
+import six
+from jazkarta.zoterolib import utils
+from jazkarta.zoterolib.testing import JAZKARTA_ZOTEROLIB_INTEGRATION_TESTING
+
+
+class ZoteroLibraryUtilsTest(unittest.TestCase):
+    def test_plone_encode(self):
+        test_str = u"Ca\u00f1amares"
+        if six.PY3:
+            self.assertIs(utils.plone_encode(test_str), test_str)
+        else:
+            self.assertEquals(utils.plone_encode(test_str), test_str.encode('utf8'))
+
+    def test_camel_case_splitter(self):
+        self.assertEquals(
+            utils.camel_case_splitter('journalArticle'), 'Journal Article'
+        )
+        self.assertEquals(
+            utils.camel_case_splitter('AFunnyThingHappenedYesterday'),
+            'A Funny Thing Happened Yesterday',
+        )
+        self.assertEquals(utils.camel_case_splitter('Two20AndSix'), 'Two20 And Six')
+
+
+class ZoteroLibraryUtilsIntegrationTest(unittest.TestCase):
+
+    layer = JAZKARTA_ZOTEROLIB_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer["portal"]
+        self.parent = self.portal
+
+    def test_html_to_plain_text(self):
+        self.assertEquals(
+            utils.html_to_plain_text('\r\n<div>Some Stuff</div>\n\n'), 'Some Stuff'
+        )
+        self.assertEquals(
+            utils.html_to_plain_text(
+                '\r\n<random>Some <spam>Stuff</spam> <br /></random>\n\n'
+            ),
+            'Some  Stuff',
+        )
+        self.assertEquals(
+            utils.html_to_plain_text(
+                '\r\n<broken>Some <spam>Stuff</span> <dr /></really_broken>\n\n'
+            ),
+            'Some  Stuff',
+        )
